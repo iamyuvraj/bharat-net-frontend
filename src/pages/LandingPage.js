fe8f14e0e49477048dc9eb73c0e1ab9ad5e08f53
Yuvraj Singh <mail.yuvrajsingh23@gmail.com>
2024-11-04 17:07:21 +0530

    Added Card for Step

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import Carousel from "../components/Carousel";
 import WhyBharatNet from "../components/WhyBharatNet";
-import RouterIcon from '@mui/icons-material/Router';
-import LoginIcon from '@mui/icons-material/Login';
-import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
-import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import RouterIcon from "@mui/icons-material/Router";
+import LoginIcon from "@mui/icons-material/Login";
+import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
 import Modal from "../components/Modal";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -246,8 +246,10 @@ const Landing = () => {
           </div>
           <div className="grid gap-8 lg:grid-cols-3 sm:gap-6 xl:gap-10">
             {/* pricing card 1 */}
-            <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow transition-transform duration-200 ease-in-out transform 
-hover:scale-105 hover:shadow-lg dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
+            <div
+              className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow transition-transform duration-200 ease-in-out transform 
+hover:scale-105 hover:shadow-lg dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white"
+            >
               <h3 className="mb-4 text-2xl font-semibold">Silver</h3>
               <p className="font-light text-gray-800 dark:text-gray-200">
                 ₹499 /month
@@ -308,8 +310,10 @@ hover:scale-105 hover:shadow-lg dark:border-gray-600 xl:p-8 dark:bg-gray-800 dar
               </a>
             </div>
             {/* pricing card 2 */}
-            <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow transition-transform duration-200 ease-in-out transform 
-hover:scale-105 hover:shadow-lg dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
+            <div
+              className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow transition-transform duration-200 ease-in-out transform 
+hover:scale-105 hover:shadow-lg dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white"
+            >
               <h3 className="mb-4 text-2xl font-semibold dark:text-yellow-500">
                 Gold
               </h3>
@@ -373,8 +377,10 @@ hover:scale-105 hover:shadow-lg dark:border-gray-600 xl:p-8 dark:bg-gray-800 dar
               </a>
             </div>
             {/* pricing card 3 */}
-            <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow transition-transform duration-200 ease-in-out transform 
-hover:scale-105 hover:shadow-lg dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
+            <div
+              className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow transition-transform duration-200 ease-in-out transform 
+hover:scale-105 hover:shadow-lg dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white"
+            >
               <h3 className="mb-4 text-2xl font-semibold dark:text-pink-500">
                 Diamond
               </h3>
@@ -460,12 +466,45 @@ hover:scale-105 hover:shadow-lg dark:border-gray-600 xl:p-8 dark:bg-gray-800 dar
       <section className="py-12 bg-gray-100 text-center">
         <h2 className="text-3xl font-semibold mb-8">How It Works</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
-          <div className="bg-white p-6 rounded-lg shadow-md">
-            <h3 className="text-xl font-semibold">Step 1: Sign Up</h3>
-            <p className="text-gray-700">
-              Register for a Bharat Net account by filling out the necessary
-              information.
-            </p>
+          <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+              <img
+                src={`${process.env.PUBLIC_URL}/other-assets/test-assets/step-two.jpg`}
+                alt="Step - 1"
+              />
+            </a>
+            <div class="p-5">
+              <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Noteworthy technology acquisitions 2021
+                </h5>
+              </a>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+              <a
+                href="#"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Read more
+                <svg
+                  class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
           </div>
 
           <div className="bg-white p-6 rounded-lg shadow-md">
